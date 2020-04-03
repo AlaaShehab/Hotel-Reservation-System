e099e67f105b3a5ac443ce32cde0164041db6175
Alaa Shehab <alaa.shehab.95@gmail.com>
2020-04-03 18:45:58 +0200

    Update AddBranch.java

--- src/Controller/AddBranch.java
+++ src/Controller/AddBranch.java
@@ -6,18 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AddBranch extends Application {
-
+public class AddBranch implements Initializable {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/addBranch.fxml"));
-        primaryStage.setTitle("Welcome to ABS Book Store");
-        primaryStage.setScene(new Scene(root, 700, 700));
-        primaryStage.show();
-    }
+    public void initialize(URL location, ResourceBundle resources) {
 
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
