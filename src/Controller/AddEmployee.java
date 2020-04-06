8b97f8c04b1a908495569a36e1c21cdbf2554e39
AyaAshrafSABER <ayasaberosman@gmail.com>
2020-04-06 03:22:59 +0200

    ADD Employee View

--- src/Controller/AddEmployee.java
+++ src/Controller/AddEmployee.java
@@ -5,10 +5,35 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddEmployee implements Initializable {
+    @FXML private Button submit;
+    @FXML private Button back;
+    @FXML private PasswordField password ;
+    @FXML private TextField firstName;
+    @FXML private TextField lastName;
+    @FXML private TextField email;
+    @FXML private TextField phoneNumber;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(
+                "../View/addEmployee.fxml"));
+        Parent root;
+        try {
+            root = (Parent) loader.load();
+        } catch (Exception e) {
+            System.out.println("cannot load");
+        }
 
     }
+    @FXML
+    private submitADDEmployee(){
+
+    }
+    @FXML
+    private goBackToHomePage(){
+
+    }
+
 }
 
